79321ac92fb74d88e6cb10d246ff7bf5846173dd
parabsunita <67011382+parabsunita@users.noreply.github.com>
2021-11-08 22:56:45 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+ 
+import { GithubService } from './github.service';
+import { repo } from './repo.model';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GitApp';
+  title="";
+  userName: string = ""
+  repo:any;
+ 
+  loading: boolean = false;
+  errorMessage:any;
+ 
+  constructor(private githubService: GithubService) {
+  }
+ //function for get git user details
+  public getRepos() {
+    this.loading = true;
+    this.errorMessage = " ";
+    this.githubService.getRepos(this.userName)
+      .subscribe(
+        (response) => {                           //next() callback
+          console.log('response received')
+          this.repo = response; 
+        },
+        (error) => {                              //error() callback
+          console.error('Request failed with error')
+          this.errorMessage = "ErrorMessage";
+          this.loading = false;
+        },
+        () => {                                   //complete() callback
+          console.error('Request completed')      //This is actually not needed 
+          this.loading = false; 
+        })
+  }
 }
+ 
